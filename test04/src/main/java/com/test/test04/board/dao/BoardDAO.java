da4413ddf047b1628456c442ab032b45aa718df7
TaeJong <dbxowhdsla12@naver.com>
2025-04-29 20:08:19 +0900

    JPA 정리

--- test04/src/main/java/com/test/test04/board/dao/BoardDAO.java
+++ test04/src/main/java/com/test/test04/board/dao/BoardDAO.java
@@ -11,10 +11,16 @@ import jakarta.persistence.PersistenceContext;
 
 @Repository
 public class BoardDAO {
+	// EntityManager를 주입받기 위한 어노테이션
+	// EntityManager 는 JPA에서 데이터베이스와의 연결 및 쿼리 실행을 담당
 	@PersistenceContext
 	private EntityManager em;
 	
 	public List<BoardDTO> boardList(){
+		// select b from Board b -> JPA의 JPQL 쿼리. Board는 @Entity(name="Board")에서 지정한 이름
+		// 여기서 Board는 실제 DB 테이블 이름 "board"와 다름 -> JPA는 @Entity(name="Board")에 정의된 엔티티 이름을 JPQL에서 사용함
+		// BoardDTO.class -> 반환할 객체 타입 (BoardDTO)
+		// getResultList() -> 결과를 리스트 형태로 가져옴
 		return em.createQuery("select b From Board b", BoardDTO.class)
 				.getResultList();
 	}
--- test04/src/main/java/com/test/test04/board/dto/BoardDTO.java
+++ test04/src/main/java/com/test/test04/board/dto/BoardDTO.java
@@ -14,15 +14,28 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+// 이 클래스가 JPA에서 관리할 엔티티 클래스임을 선언.
+// 테이터베이스 테이블과 매핑됨
 @Entity(name="Board")
+
+// 실제 매핑될 테이블 이름 지정
 @Table(name="board")
+
+// Spring Bean으로 등록
 @Component
 @Setter
 @Getter
 @ToString
 public class BoardDTO {
+	// 해당 필드가 Primary Key 임을 지정
 	@Id
+	
+	// 기본 키 생성 전략 설정. 여기선 SEQUENCE 전략을 사용
+	// 시퀀스를 이용한 자동 번호 생성 방식
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="board_articleNo")
+	
+	// 실제 DB에 존재하는 시퀀스 이름
+	// DB에 만들어져 있어야 함
 	@SequenceGenerator(name="board_articleNo", sequenceName="board_articleNo", 
 	allocationSize=1)
 	private int articleNo;
